0ec42e9ed243d2687f8e7d129d3fd8fddd39ea56
dekkyy1 <dekkyyd@gmail.com>
2013-10-11 13:57:25 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/htc/m7ul/full_m7ul.mk)
 
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cosconfig/gsm.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cos/config/nfc_enhanced.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="htc_europe/m7/m7:4.1.2/JZO54K/177101.12:user/release-keys" PRIVATE_BUILD_DESC="1.29.401.12 CL177101 release-keys"
 
-PRODUCT_NAME := cm_m7ul
+PRODUCT_NAME := cos_m7ul
 PRODUCT_DEVICE := m7ul
